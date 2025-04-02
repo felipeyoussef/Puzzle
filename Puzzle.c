db6c722587f8e85457487aaf30f0ff92f28122f1
Felipe/Silva <a109385@alunos.uminho.pt>
2025-04-02 17:48:05 +0100

    Simplificação das variáveis, feature:Sair

--- Puzzle.c
+++ Puzzle.c
@@ -3,38 +3,36 @@
 
 typedef struct {
     char titulo[50]; //nome do jogo
-    char opcoes[4][50]; //opcoes jogar, sair 
+    char opcoes[5][50]; //opcoes jogar, sair 
     int num_opcoes; //quantas opcoes existem
     int selecionada; //qual opcao esta selecionada
 } telainicio;
 
-telainicio tela = {
-    "Puzzle Terminal",
-    {"Jogar", "Carregar jogo salvo", "Créditos", "Sair"},
-    4,
-    0  // começa com a primeira opção selecionada
-};
+telainicio telainicial = {"Puzzle Terminal", {"Jogar", "Carregar jogo salvo", "Salvar Jogo atual", "Créditos", "Sair"}, 5, 0};//Definindo os nomes das telas, quantidade de telas, e primeira tela selecionada
 
-void desenhaTela(telainicio tela) {
+void desenhaTela(telainicio telainicial) {
     system("clear");
-    printf("===%s===\n\n", tela.titulo);
-    for (int i = 0; i < tela.num_opcoes; i++) {
-        if (i == tela.selecionada)
-            printf("> %s <\n", tela.opcoes[i]);
+    printf("===%s===\n\n", telainicial.titulo);
+    for (int i = 0; i < telainicial.num_opcoes; i++) {
+        if (i == telainicial.selecionada)
+            printf("> %s <\n", telainicial.opcoes[i]);
         else
-            printf("    %s\n", tela.opcoes[i]);
+            printf("%s\n", telainicial.opcoes[i]);
     }
+    printf("\n\nPara acessar qualquer tela, aperte P e ENTER!\n\n");
 };
 
 int main() {
     char tecla;
     do {
-        desenhaTela(tela);
+        desenhaTela(telainicial);
         tecla = getchar();
         getchar(); // captura o '\n' que sobra no buffer
-        if (tecla == 'w' && tela.selecionada > 0)
-            tela.selecionada--;
-        else if (tecla == 's' && tela.selecionada < tela.num_opcoes - 1)
-            tela.selecionada++;
+        if (tecla == 'w' && telainicial.selecionada > 0)
+            telainicial.selecionada--; //Subir a escolha de tela
+        else if (tecla == 's' && telainicial.selecionada < telainicial.num_opcoes - 1)
+            telainicial.selecionada++; //Descer a escolha de tela
+        else if (tecla == 'p' && telainicial.selecionada == 4)
+            break; //Seleciona o Sair e com P, sai do jogo
     } while (tecla != '\n');
 }
