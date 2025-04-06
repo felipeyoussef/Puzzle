5715759de12d0e7c2047a0f8638de92c7c3fd451
Felipe/Silva <a109385@alunos.uminho.pt>
2025-04-06 18:11:06 +0100

    feat: carregarJogo + exibeJogo

--- Puzzle.c
+++ Puzzle.c
@@ -22,17 +22,58 @@ void desenhaTela(telainicio telainicial) {
     printf("\n\nPara acessar qualquer tela, aperte P e ENTER!\n\n");
 };
 
+
+typedef struct{
+    int linhas;
+    int colunas;
+    char **matriz;
+} Jogo;
+
+
+void carregarJogo(Jogo *meuJogo, const char *nome_arquivo){
+    FILE *arquivo = fopen(nome_arquivo, "r");
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo, tente novamente!\n");
+        exit(1);
+    }
+    fscanf(arquivo, "%d %d", &meuJogo->linhas, &meuJogo->colunas);
+    meuJogo->matriz = malloc(meuJogo->linhas * sizeof(char*));
+    for (int i = 0; i< meuJogo->linhas; i++)
+        meuJogo->matriz[i] = malloc(meuJogo->colunas * sizeof(char));
+
+    fclose(arquivo);
+}
+
+void exibeJogo(Jogo meuJogo){
+    for(int i = 0; i < meuJogo.linhas; i++){
+        for(int j = 0; j < meuJogo.colunas; j++){
+            printf("%c", meuJogo.matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     char tecla;
-    do {
+    Jogo meuJogo;
+    while (1) {
         desenhaTela(telainicial);
         tecla = getchar();
-        getchar(); // captura o '\n' que sobra no buffer
+        getchar(); // lê o '\n' que fica no buffer
+
         if (tecla == 'w' && telainicial.selecionada > 0)
-            telainicial.selecionada--; //Subir a escolha de tela
+            telainicial.selecionada--; // Sobe
         else if (tecla == 's' && telainicial.selecionada < telainicial.num_opcoes - 1)
-            telainicial.selecionada++; //Descer a escolha de tela
-        else if (tecla == 'p' && telainicial.selecionada == 4)
-            break; //Seleciona o Sair e com P, sai do jogo
-    } while (tecla != '\n');
+            telainicial.selecionada++; // Desce
+        else if (tecla == 'p') {
+            if (telainicial.selecionada == 4)
+                break; // Sai do jogo
+            // Aqui podes tratar outras opções também
+            else if(telainicial.selecionada == 1)
+                carregarJogo(&meuJogo, "jogo.txt");
+
+        }
+    }
+
+    return 0;
 }
