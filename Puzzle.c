2e95d9bc891c74eabb48a1f7ee1fd765e90aab89
Felipe/Silva <a109385@alunos.uminho.pt>
2025-04-07 12:43:30 +0100

    Versão final 1.0

--- Puzzle.c
+++ Puzzle.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct {
     char titulo[50]; //nome do jogo
@@ -19,17 +21,39 @@ void desenhaTela(telainicio telainicial) { //DESENHO DO MENU
         else
             printf("%s\n", telainicial.opcoes[i]); //exibe as outras opcoes
     }
+<<<<<<< Updated upstream
     printf("\n\nPara navegar pelo menu, use S para descer e W para subir!");
     printf("\nPara acessar qualquer tela, aperte P e ENTER!\n\n");
+=======
+    printf("\nUse W e S para navegar pelas opções\n");
+     printf("Pressione P e ENTER para acessar a tela!\n\n");
+>>>>>>> Stashed changes
 };
 
 
 typedef struct{ //struct do arquivo de jogo
-    int linhas;
-    int colunas;
-    char **matriz;
+    int linhas; //qntd de linhas
+    int colunas; //qntd de colunas
+    char **matriz; //tabuleiro do jogo
+    char **entrada; //input do jogador
 } Jogo;
 
+<<<<<<< Updated upstream
+=======
+     printf("  pintar <linha> <coluna> <tipo>\n");
+     printf("    - Marca uma célula na posição indicada.\n");
+     printf("    - <tipo> pode ser:\n");
+     printf("        branca   → limpa a célula (representada por 'EX:a -> A')\n");
+     printf("        riscada  → marca a célula (representada por '#')\n\n");
+
+     printf("  sair\n");
+     printf("    - Volta para o menu principal\n\n");
+
+     printf("----------------------------------\n");
+     printf("Ah! E durante o menu principal:\n");
+     getchar(); // espera o usuário ler
+}
+>>>>>>> Stashed changes
 
 void carregarJogo(Jogo *meuJogo, const char *nome_arquivo){ //funcao para ler e escrever o arquivo jogo.txt para jogarmos posteriormente
     printf("Abrindo jogo: %s\n", nome_arquivo);
@@ -62,6 +86,7 @@ void exibeJogo(Jogo *meuJogo){
     }
 }
 
+<<<<<<< Updated upstream
 int main() {
     char tecla;
     Jogo meuJogo;
@@ -69,6 +94,97 @@ int main() {
         desenhaTela(telainicial);
         tecla = getchar();
         getchar();
+=======
+// MECANICA DO JOGO
+void modoInterativo(Jogo *meuJogo) {
+    int comandoAtual = 0;
+    meuJogo->entrada = malloc(100 * sizeof(char*)); // através do **entrada e desse malloc, da para implementar a ação de voltar comandos
+
+    for (int i = 0; i < 100; i++) {
+        meuJogo->entrada[i] = malloc(100 * sizeof(char)); // alocar memoria para cada comando
+    }
+
+    
+
+    printf("\n>> Modo interativo iniciado! (digite 'sair' pra voltar pro menu)\n");
+
+    while (1) {
+        printf("\n> ");
+        fgets(meuJogo->entrada[comandoAtual], 100, stdin);
+        meuJogo->entrada[comandoAtual][strcspn(meuJogo->entrada[comandoAtual], "\n")] = '\0';
+
+        if (strcmp(meuJogo->entrada[comandoAtual], "sair") == 0)
+            break;
+
+        char *comando = strtok(meuJogo->entrada[comandoAtual], " ");
+
+        if (comando && strcmp(comando, "pintar") == 0) {
+            char *strX = strtok(NULL, " ");
+            char *strY = strtok(NULL, " ");
+            char *tipoCelula = strtok(NULL, " ");
+
+            if (strX && strY && tipoCelula) {
+                int linha = atoi(strX);
+                int coluna = atoi(strY);
+
+                if (linha >= 0 && linha < meuJogo->linhas && coluna >= 0 && coluna < meuJogo->colunas) {
+                    if (strcmp(tipoCelula, "branca") == 0) {
+                        char letra = meuJogo->matriz[linha][coluna];
+                        meuJogo->matriz[linha][coluna] = toupper(letra);
+
+                        for (int j = 0; j < meuJogo->colunas; j++) {
+                            if (j != coluna && meuJogo->matriz[linha][j] == letra)
+                                meuJogo->matriz[linha][j] = '#';
+                        }
+
+                        for (int i = 0; i < meuJogo->linhas; i++) {
+                            if (i != linha && meuJogo->matriz[i][coluna] == letra)
+                                meuJogo->matriz[i][coluna] = '#';
+                        }
+
+                    } else if (strcmp(tipoCelula, "riscada") == 0) {
+                        char letra = meuJogo->matriz[linha][coluna];
+                        meuJogo->matriz[linha][coluna] = '#';
+
+                        for (int j = 0; j < meuJogo->colunas; j++) {
+                            if (j != coluna && meuJogo->matriz[linha][j] == letra)
+                                meuJogo->matriz[linha][j] = '#';
+                        }
+
+                        for (int i = 0; i < meuJogo->linhas; i++) {
+                            if (i != linha && meuJogo->matriz[i][coluna] == letra)
+                                meuJogo->matriz[i][coluna] = '#';
+                        }
+
+                    } else {
+                        printf("Tipo inválido! Use 'branca' ou 'riscada'.\n");
+                    }
+                } else {
+                    printf("Opa, coordenadas fora do tabuleiro!\n");
+                }
+            } else {
+                printf("Formato esperado: pintar <linha> <coluna> <branca|riscada>\n");
+            }
+
+        } else {
+            printf("Comando desconhecido. Tenta 'pintar' ou 'sair'.\n");
+        }
+
+        exibeJogo(meuJogo);
+        comandoAtual++; // registra o próximo comando
+    }
+}
+
+//salvarJogo(Jogo *meuJogo){ABRIR FICHEIRO NOVO COM FILE, FPRINTF PARA ESCREVER TODOS OS ELEMENTOS DO JOGO PARA O OUTRO FICHEIRO}
+
+int main() {
+    char tecla;
+    Jogo meuJogo; // ⚠️ Precisavas declarar isso
+    do {
+        desenhaTela(telainicial);
+        tecla = getchar();
+        getchar(); // lê o '\n'
+>>>>>>> Stashed changes
 
         if (tecla == 'w' && telainicial.selecionada > 0)
             telainicial.selecionada--; // Sobe
@@ -79,6 +195,7 @@ int main() {
                 system("clear");
                 break; // Sai do jogo
             }
+<<<<<<< Updated upstream
             else if(telainicial.selecionada == 1){ //CARREGAR, já está pronto para a feat de salvar jogo!!
                 carregarJogo(&meuJogo, "jogo.txt");
                 exibeJogo(&meuJogo);
@@ -90,6 +207,24 @@ int main() {
             }
         }
     }
+=======
+            else if (telainicial.selecionada == 1) { // CARREGAR
+                carregarJogo(&meuJogo, "jogo.txt");
+                exibeJogo(&meuJogo);
+                printf("\nJogo Carregado, Pressione ENTER para voltar no MENU e jogar!");
+                getchar();
+            }
+            else if (telainicial.selecionada == 0) { // JOGAR
+                mostrarControle();
+                exibeJogo(&meuJogo);
+                printf("\nPressione ENTER para entrar no modo jogo!");
+                getchar();
+                modoInterativo(&meuJogo);
+            }
+        }
+
+    } while (1); // loop principal correto
+>>>>>>> Stashed changes
 
     return 0;
 }
